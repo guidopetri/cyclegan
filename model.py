a42d3c1723b98a7ac7898a2a816990e07af139d1
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-11-12 22:38:09 -0500

    cyclegan model as per the paper

---
+++ model.py
@@ -0,0 +1,124 @@
+#! /usr/bin/env python3
+
+import torch
+from utils import ResidualBlock, cyclegan_loss
+
+
+class CycleGAN(torch.nn.Module):
+
+    # original network is:
+    # c7s1-64,d128,d256,R256,R256,R256,
+    # R256,R256,R256,u128,u64,c7s1-3
+
+    def __init__(self, num_residual_blocks=6, size=256):
+        super().__init__()
+
+        self.activation = torch.nn.ReLU()
+
+        # c7s1-64
+        self.conv1 = torch.nn.Conv2d(3,
+                                     64,
+                                     kernel_size=7,
+                                     stride=1,
+                                     padding=3,
+                                     padding_mode='reflect',
+                                     )
+        self.norm_64 = torch.nn.InstanceNorm2d(num_features=64)
+
+        # d128
+        self.conv2 = torch.nn.Conv2d(64,
+                                     128,
+                                     kernel_size=3,
+                                     stride=2,
+                                     padding=1,
+                                     padding_mode='reflect',
+                                     )
+        self.norm_128 = torch.nn.InstanceNorm2d(num_features=128)
+
+        # d256
+        self.conv3 = torch.nn.Conv2d(128,
+                                     256,
+                                     kernel_size=3,
+                                     stride=2,
+                                     padding=1,
+                                     padding_mode='reflect',
+                                     )
+        self.norm_256 = torch.nn.InstanceNorm2d(num_features=256)
+
+        # R's
+        self.residual_blocks = torch.nn.ModuleList()
+
+        for _ in range(num_residual_blocks):
+            residual_block = ResidualBlock(size)
+
+            self.residual_blocks.append(residual_block)
+
+        # u128
+        # somehow, i think this is correct
+        self.frac_conv1 = torch.nn.ConvTranspose2d(256,
+                                                   128,
+                                                   kernel_size=3,
+                                                   stride=2,
+                                                   padding=1,
+                                                   output_padding=1,
+                                                   )
+
+        # u64
+        self.frac_conv2 = torch.nn.ConvTranspose2d(128,
+                                                   64,
+                                                   kernel_size=3,
+                                                   stride=2,
+                                                   padding=1,
+                                                   output_padding=1,
+                                                   )
+
+        # c7s1-3
+        self.conv4 = torch.nn.Conv2d(64,
+                                     3,
+                                     kernel_size=7,
+                                     stride=1,
+                                     padding=3,
+                                     padding_mode='reflect',
+                                     )
+        self.norm_3 = torch.nn.InstanceNorm2d(num_features=3)
+
+    def forward(self, x):
+        
+        x = self.conv1(x)
+        x = self.norm_64(x)
+        x = self.activation(x)
+
+        x = self.conv2(x)
+        x = self.norm_128(x)
+        x = self.activation(x)
+
+        x = self.conv3(x)
+        x = self.norm_256(x)
+        x = self.activation(x)
+
+        for res_block in self.residual_blocks:
+            x = res_block(x)
+
+        x = self.frac_conv1(x)
+        x = self.norm_128(x)
+        x = self.activation(x)
+
+        x = self.frac_conv2(x)
+        x = self.norm_64(x)
+        x = self.activation(x)
+
+        x = self.conv4(x)
+        x = self.norm_3(x)
+        x = self.activation(x)
+
+        return x
+
+
+if __name__ == '__main__':
+    model = CycleGAN()
+
+    t = torch.rand(size=(1, 3, 128, 128))
+
+    output = model(t)
+
+    print(output.shape)
