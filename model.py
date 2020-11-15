00519e58724fc21a5bda9dc1ace4035f04b12f64
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-11-15 15:28:47 -0500

    add patchgan architecture

--- model.py
+++ model.py
@@ -114,10 +114,96 @@ class CycleGAN(torch.nn.Module):
         return x
 
 
+class PatchGAN(torch.nn.Module):
+
+    # based off the paper description
+    # original network is:
+    # C64-C128-C256-C512
+
+    def __init__(self):
+        super().__init__()
+
+        self.activation = torch.nn.LeakyReLU(negative_slope=0.2)
+        
+        # C64
+        self.conv1 = torch.nn.Conv2d(3,
+                                     64,
+                                     kernel_size=4,
+                                     stride=2,
+                                     )
+        # skip the norm for this layer
+
+        # C128
+        self.conv2 = torch.nn.Conv2d(64,
+                                     128,
+                                     kernel_size=4,
+                                     stride=2,
+                                     )
+        self.norm_128 = torch.nn.InstanceNorm2d(num_features=128)
+
+        # C256
+        self.conv3 = torch.nn.Conv2d(128,
+                                     256,
+                                     kernel_size=4,
+                                     stride=2,
+                                     )
+        self.norm_256 = torch.nn.InstanceNorm2d(num_features=256)
+
+        # C512
+        self.conv4 = torch.nn.Conv2d(256,
+                                     512,
+                                     kernel_size=4,
+                                     stride=2,
+                                     )
+        self.norm_512 = torch.nn.InstanceNorm2d(num_features=512)
+
+        # produce 1-dimensional output
+
+        self.conv_final = torch.nn.Conv2d(512,
+                                          1,
+                                          kernel_size=2,
+                                          )
+
+        # sigmoid at the end, according to https://arxiv.org/pdf/1611.07004.pdf
+
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = self.activation(x)
+
+        x = self.conv2(x)
+        x = self.norm_128(x)
+        x = self.activation(x)
+
+        x = self.conv3(x)
+        x = self.norm_256(x)
+        x = self.activation(x)
+
+        x = self.conv4(x)
+        x = self.norm_512(x)
+        x = self.activation(x)
+
+        x = self.conv_final(x)
+
+        x = self.sigmoid(x)
+
+        return x.squeeze()
+
+
 if __name__ == '__main__':
     model = CycleGAN()
 
     t = torch.rand(size=(1, 3, 128, 128))
+
+    output = model(t)
+
+    print(output.shape)
+
+    model = PatchGAN()
+
+    t = torch.rand(size=(5, 3, 70, 70))
 
     output = model(t)
 
