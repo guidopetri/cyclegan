dea4617ab4042a181572316632f0e0e410793c5f
Ross Bernstein <rb2540@nyu.edu>
2020-12-06 20:27:01 -0500

    added necessary functions for visdom and logging

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python3
 
-"""
-Code for CycleGAN loss functions and the basic residual blocks that make up
-the network.
-"""
-
 import torch
 import torch.nn.functional as F
-
+from visdom import Visdom
+import sys
+import time
+import datetime
+import numpy as np
+import random
 
 def adversarial_loss(discriminator, output, target):
     first = torch.log(discriminator(target))
@@ -39,49 +39,130 @@ def cyclegan_loss(D_g, D_f, G, F, x, y):
     return gan_loss_1 + gan_loss_2 + cycle + identity
 
 
-class ResidualBlock(torch.nn.Module):
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+        
 
-    def __init__(self, size):
-        super().__init__()
+def init_weights(net, init_type='normal', gain=0.02):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            init.normal(m.weight.data, 0.0, gain)
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant(m.bias.data, 0.0)
+        elif classname.find('BatchNorm2d') != -1:
+            init.normal(m.weight.data, 1.0, gain)
+            init.constant(m.bias.data, 0.0)
 
-        # two 3x3 conv layers (with same number of filters: 256)
-        # batch norm? or instance norm? for batch size=1, it's the same
-        # but which to use... probably instance norm is best for
-        # reimplementation
+    print('Network initialized with weights sampled from N(0,0.02).')
+    net.apply(init_func)
 
-        # uses ReLU internally, and does:
-        # input - conv - norm - relu - conv - norm - addition - relu - output
+    
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    elif classname.find("BatchNorm") != -1:
+        torch.nn.init.normal_(m.weight, 1.0, 0.02)
+        torch.nn.init.zeros_(m.bias)
+        
+        
+class LambdaLR():
+    def __init__(self, n_epochs, offset, decay_epoch):
+        self.n_epochs = n_epochs
+        self.offset = offset
+        self.decay_epoch = decay_epoch
 
-        # reference:
-        # http://torch.ch/blog/2016/02/04/resnets.html
+    def step(self, epoch):
+        return 1.0 - max(0, epoch + self.offset - self.decay_epoch)/(self.n_epochs - self.decay_epoch)
 
-        self.size = size
+    
+class ReplayBuffer():
+    def __init__(self, max_size=50):
+        assert (max_size > 0), "Empty buffer"
+        self.max_size = max_size
+        self.data = []
 
-        self.conv1 = torch.nn.Conv2d(self.size,
-                                     self.size,
-                                     kernel_size=3,
-                                     padding=1,
-                                     padding_mode='reflect',
-                                     )
-        self.conv2 = torch.nn.Conv2d(self.size,
-                                     self.size,
-                                     kernel_size=3,
-                                     padding=1,
-                                     padding_mode='reflect',
-                                     )
+    def push_and_pop(self, data):
+        to_return = []
+        for element in data.data:
+            element = torch.unsqueeze(element, 0)
+            if len(self.data) < self.max_size:
+                self.data.append(element)
+                to_return.append(element)
+            else:
+                if random.uniform(0, 1) > 0.5:
+                    i = random.randint(0, self.max_size - 1)
+                    to_return.append(self.data[i].clone())
+                    self.data[i] = element
+                else:
+                    to_return.append(element)
+        return torch.cat(to_return)
+    
+    
+def tensor2image(tensor):
+    image = 127.5*(tensor[0].cpu().float().numpy() + 1.0)
+    if image.shape[0] == 1:
+        image = np.tile(image, (3,1,1))
+    return image.astype(np.uint8)
+      
+    
+class Logger():
+    def __init__(self, n_epochs, batches_epoch):
+        self.viz = Visdom()
+        self.n_epochs = n_epochs
+        self.batches_epoch = batches_epoch
+        self.epoch = 1
+        self.batch = 1
+        self.prev_time = time.time()
+        self.mean_period = 0
+        self.losses = {}
+        self.loss_windows = {}
+        self.image_windows = {}
 
-        self.activation = torch.nn.ReLU()
-        self.norm = torch.nn.InstanceNorm2d(num_features=self.size)
 
-    def forward(self, x):
-        y = self.conv1(x)
-        y = self.norm(y)
-        y = self.activation(y)
+    def log(self, losses=None, images=None):
+        self.mean_period += (time.time() - self.prev_time)
+        self.prev_time = time.time()
 
-        y = self.conv2(y)
-        y = self.norm(y)
-        # the "residual" part
-        y += x
-        y = self.activation(y)
+        for i, loss_name in enumerate(losses.keys()):
+            if loss_name not in self.losses:
+                self.losses[loss_name] = losses[loss_name].item()
+            else:
+                self.losses[loss_name] += losses[loss_name].item()
 
-        return y
+        batches_done = self.batches_epoch*(self.epoch - 1) + self.batch
+        batches_left = self.batches_epoch*(self.n_epochs - self.epoch) + self.batches_epoch - self.batch 
+
+        # Draw images
+        for image_name, tensor in images.items():
+            if image_name not in self.image_windows:
+                self.image_windows[image_name] = self.viz.image(tensor2image(tensor.data), opts={'title':image_name})
+            else:
+                self.viz.image(tensor2image(tensor.data), win=self.image_windows[image_name], opts={'title':image_name})
+
+        # End of epoch
+        if (self.batch % self.batches_epoch) == 0:
+            # Plot losses
+            for loss_name, loss in self.losses.items():
+                if loss_name not in self.loss_windows:
+                    self.loss_windows[loss_name] = self.viz.line(X=np.array([self.epoch]), Y=np.array([loss/self.batch]), 
+                                                                    opts={'xlabel': 'epochs', 'ylabel': loss_name, 'title': loss_name})
+                else:
+                    self.viz.line(X=np.array([self.epoch]), Y=np.array([loss/self.batch]), win=self.loss_windows[loss_name], update='append')
+                # Reset losses for next epoch
+                self.losses[loss_name] = 0.0
+
+            self.epoch += 1
+            self.batch = 1
+            sys.stdout.write('\n')
+        else:
+            self.batch += 1
+            
